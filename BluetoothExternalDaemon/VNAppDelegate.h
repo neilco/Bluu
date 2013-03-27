c5bf75db497ad0415b0cce3209c74d44e9a44dfc
Neil Cowburn <neilco@gmail.com>
2013-03-27 20:06:20 +0000

    Initial Commit

---
+++ BluetoothExternalDaemon/VNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VNAppDelegate.h
+//  BluetoothExternalDaemon
+//
+//  Created by Neil on 27/03/2013.
+//  Copyright (c) 2013 Vividnoise. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VNAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
