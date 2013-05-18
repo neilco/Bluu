7470a4629a89b84ad3cf3ce01ab2d24447420cbd
Neil Cowburn <neilco@gmail.com>
2013-05-18 15:50:25 +0100

    Renamed icon

--- Bluu/main.m
+++ Bluu/main.m
@@ -3,7 +3,7 @@
 //  BluetoothExternalDaemon
 //
 //  Created by Neil on 27/03/2013.
-//  Copyright (c) 2013 Vividnoise. All rights reserved.
+//  Copyright (c) 2013 Neil Cowburn. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
