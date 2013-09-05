894fa729bc318a32b7a91eec59469e16df810996
Neil Cowburn <neilco@gmail.com>
2013-09-05 22:21:24 +0100

    Fixed bug that incorrectly disabled Bluetooth

--- Bluu/AppDelegate.m
+++ Bluu/AppDelegate.m
@@ -12,6 +12,7 @@
 
 NSString *const kDisplayNotifications = @"displayNotifications";
 NSString *const kBTExtDAppID = @"com.neilcowburn.btextd";
+NSString *const kScreenCount = @"screenCount";
 
 @interface AppDelegate () <NSUserNotificationCenterDelegate>
 
@@ -36,27 +37,42 @@ NSString *const kBTExtDAppID = @"com.neilcowburn.btextd";
     [NCPreferences setAppID:kBTExtDAppID];
     BOOL showNotification = [[NCPreferences sharedPreferences] boolForKey:kDisplayNotifications];
     
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSUInteger lastScreenCount = (NSUInteger)[defaults integerForKey:kScreenCount];
+    
     NSArray *screens = [NSScreen screens];
-    for (NSScreen *screen in screens) {
-        CGDirectDisplayID screenID = [[[screen deviceDescription] objectForKey:@"NSScreenNumber"] unsignedIntValue];
-        if (screens.count == 1 && CGDisplayIsBuiltin(screenID)) {
-            if (IOBluetoothPreferenceGetControllerPowerState() == 1) {
-                IOBluetoothPreferenceSetControllerPowerState(0); // Turn BT off
-                if (showNotification) {
-                    [self showNotification:NO];
+    if (screens.count != lastScreenCount) {
+        for (NSScreen *screen in screens) {
+            CGDirectDisplayID screenID = [[[screen deviceDescription] objectForKey:@"NSScreenNumber"] unsignedIntValue];
+            if (screens.count == 1 && CGDisplayIsBuiltin(screenID)) {
+                if (IOBluetoothPreferenceGetControllerPowerState() == 1) {
+                    IOBluetoothPreferenceSetControllerPowerState(0); // Turn BT off
+                    if (showNotification) {
+                        [self showNotification:NO];
+                    }
                 }
-            }
-            break;
-        } else if (screens.count >= 1 && !CGDisplayIsBuiltin(screenID)) { // First external display
-            if (IOBluetoothPreferenceGetControllerPowerState() == 0) {
-                IOBluetoothPreferenceSetControllerPowerState(1); // Turn BT on
-                if (showNotification) {
-                    [self showNotification:YES];
+                break;
+            } else if (screens.count >= 1 && !CGDisplayIsBuiltin(screenID)) { // First external display
+                if (IOBluetoothPreferenceGetControllerPowerState() == 0) {
+                    IOBluetoothPreferenceSetControllerPowerState(1); // Turn BT on
+                    if (showNotification) {
+                        [self showNotification:YES];
+                    }
                 }
+                break;
             }
-            break;
         }
+        
+        [defaults setInteger:screens.count forKey:kScreenCount];
+        [defaults synchronize];
     }
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults registerDefaults:@{ kScreenCount: @1 }];
+    [defaults synchronize];
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification
